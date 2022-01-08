c7a63f33e8268c02a0621d43671b3d2dc5c1e1f1
Kyle Silver <kyleAsilver@gmail.com>
2022-01-08 15:22:07 -0500

    forgot about unwrap

--- src/main/java/dev/kylesilver/result/UnwrapException.java
+++ src/main/java/dev/kylesilver/result/UnwrapException.java
@@ -1,5 +1,16 @@
 package dev.kylesilver.result;
 
+import java.util.function.Function;
+
+/**
+ * This exception is thrown when a value is unwrapped. Unwrapping means that the
+ * {@link Result} was not what was expected and should be treated as fatal. It
+ * is a checked exception to discourage the practice of catching unwraps. To
+ * avoid muddying your method signatures with this exception, consider supplying
+ * a default value or coming up with a way to gracefully handle exceptions. You
+ * can also use the {@link Result#expect(Function)} method to throw a custom
+ * error instead of this one.
+ */
 public class UnwrapException extends Exception {
     public UnwrapException(String s) {
         super(s);
